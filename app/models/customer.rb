04d32699285ae4727fbb1f914cf6bf6eb811caec
Camille Bettini <camille_bettini@icloud.com>
2019-09-03 23:04:30 -0300

    [add] name save as titleize

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,11 @@
 class Customer < ApplicationRecord
   has_many :carts
+
+  before_save :transform_name
+
+  def transform_name
+    transformed_name = name.titleize
+
+    write_attribute(:name, transformed_name)
+  end
 end
