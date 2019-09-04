c5571feb0cfdd79efe8075146661538b09d5f35d
Camille Bettini <camille_bettini@icloud.com>
2019-09-03 23:46:55 -0300

    [add] total cart price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ApplicationRecord
   belongs_to :customer
   has_many :cart_items
+
+  def total_cart_price
+    total = 0
+    CartItem.all.map do |item|
+      med = Medicine.find(item.medicine_id)
+      total = item.quantity * med.value
+    end
+
+    total
+  end
 end
