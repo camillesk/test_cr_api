33116affe77f388968a2d789d4c52a041e36fdc7
Camille Bettini <camille_bettini@icloud.com>
2019-09-03 23:38:13 -0300

    [add] add to cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,30 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :medicine
+
+  validates :cart_id, :medicine_id, :quantity, presence: true
+  validate :stock_positive, :stock_qty
+
+  after_save :remove_from_stock
+
+  private
+
+  def stock_positive
+    med = Medicine.find(medicine_id)
+
+    errors.add(:quantity, 'Out of order') unless med.stock.positive?
+  end
+
+  def stock_qty
+    med = Medicine.find(medicine_id)
+
+    errors.add(:quantity, 'Not enough stock') unless (med.stock - quantity).positive?
+  end
+
+  def remove_from_stock
+    med = Medicine.find(medicine_id)
+
+    med.stock -= quantity
+    med.save
+  end
 end
